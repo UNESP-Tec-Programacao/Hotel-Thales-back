e81b0335e55fb4375ed683806513c74134c85bc2
EduardoMRT <eduksoficial@gmail.com>
2025-04-23 10:26:58 -0300

    Including base project

---
+++ Thales-Hotel/src/main/java/br/com/unesp/Thales_Hotel/ThalesHotelApplication.java
@@ -0,0 +1,13 @@
+package br.com.unesp.Thales_Hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThalesHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThalesHotelApplication.class, args);
+	}
+
+}
---
+++ Thales-Hotel/src/test/java/br/com/unesp/Thales_Hotel/ThalesHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unesp.Thales_Hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThalesHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
